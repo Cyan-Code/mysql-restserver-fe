eab7042a2d6249b506792382230595d8b3ed3574
Luis Carrion <lcarrionalvarez@gmail.com>
2022-02-09 14:32:32 -0500

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
+interface Usuarios {
+  usuarios: Usuario[] | []
+}
+
+interface Usuario {
+  nombre: string;
+  email: string;
+  password: string;
+  estado: boolean;
+  createdAt: string;
+  updatedAt: string;
+}
+
+function App () {
+  
+  const [usuarios, setUser] = useState<Usuarios>()
+  
+  useEffect(() => {
+    fetch('http://localhost:8000/api/usuarios/', {
+      method: 'GET'
+    })
+    .then(resp => resp.json())
+    .then(resp => setUser(resp))
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <div className="container">
+      <div className="d-flex bd-highlight mb-3">
+        <div className="me-auto p-2 bd-highlight">Users</div>
+        <div className="p-2 bd-highlight">
+          <button type="button" className="btn btn-secondary">Create</button>
+        </div>
+      </div>
+      
+      <div className="table-responsive">
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Nombre</th>
+              <th scope="col">Correo</th>
+              <th scope="col">Password</th>
+            </tr>
+          </thead>
+          <tbody id="mytable">
+            <tr>
+              <td>he</td>
+              <td>hi</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div> 
+    </>
   );
 }
 
